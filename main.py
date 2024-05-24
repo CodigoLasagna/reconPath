c0d8e8ab72b2d0ff1ad3d35ed87b9dd8c208f84c
Hanck Eddison <hanckeddison@gmail.com>
2024-05-23 18:28:19 -0700

    models

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gesture_detection import HandGestureDetector
 from gestKnn_module import HandGestureClassifierKnn
 
 if __name__ == "__main__":
-    classifier = HandGestureClassifierKnn('hand_gesture_dataset/labels.csv', 'model.pkl', 'model_2.pkl')
+    classifier = HandGestureClassifierKnn('hand_gesture_dataset/labels.csv', 'model_8.pkl', 'model_8.pkl')
     detector = HandGestureDetector(max_num_hands=2, min_detection_confidence=0.7, auto_word='', classifier=classifier)
 
     detector.detect_gestures()
